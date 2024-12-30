46944757318186a20c2468982d3acac45e2b1367
mmktomato <mkt.motohashi@gmail.com>
2024-12-30 14:46:40 +0900

    Improve event type

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,41 @@
-export { type ClickWheelerRotateEvent, type ClickWheelerTapEvent } from "./click-wheeler/events";
+import { type ClickWheelerRotateEvent, type ClickWheelerTapEvent } from "./click-wheeler/events";
+export { type ClickWheelerRotateEvent, type ClickWheelerTapEvent };
 
 export declare class HTMLClickWheelerElement extends HTMLElement {
   size?: number;
   requireShiftToRotate?: boolean;
   constructor();
+
+  addEventListener(
+    type: "rotate",
+    listener: (event: ClickWheelerRotateEvent) => void,
+    options?: boolean | AddEventListenerOptions,
+  ): void;
+  addEventListener(
+    type: "tap",
+    listener: (event: ClickWheelerTapEvent) => void,
+    options?: boolean | AddEventListenerOptions,
+  ): void;
+  addEventListener<K extends keyof HTMLElementEventMap>(
+    type: K,
+    listener: (this: HTMLElement, ev: HTMLElementEventMap[K]) => any,
+    options?: boolean | AddEventListenerOptions,
+  ): void;
+  removeEventListener(
+    type: "rotate",
+    listener: (event: ClickWheelerRotateEvent) => void,
+    options?: boolean | AddEventListenerOptions,
+  ): void;
+  removeEventListener(
+    type: "tap",
+    listener: (event: ClickWheelerTapEvent) => void,
+    options?: boolean | AddEventListenerOptions,
+  ): void;
+  removeEventListener<K extends keyof HTMLElementEventMap>(
+    type: K,
+    listener: (this: HTMLElement, ev: HTMLElementEventMap[K]) => any,
+    options?: boolean | AddEventListenerOptions,
+  ): void;
 }
 
 interface ClickWheelerProps {
